2ca7ad316cd772c2f55620396c03443dac917127
to <>
2018-10-02 18:38:42 +0900

    use gson

--- app/src/main/java/com/yt8492/ircontrol/MainActivity.kt
+++ app/src/main/java/com/yt8492/ircontrol/MainActivity.kt
@@ -3,13 +3,19 @@ package com.yt8492.ircontrol
 import android.app.Activity
 import android.app.AlertDialog
 import android.app.Dialog
+import android.content.Context
 import android.content.DialogInterface
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.widget.EditText
 import com.google.android.things.pio.Gpio
 import com.google.android.things.pio.GpioCallback
 import com.google.android.things.pio.PeripheralManager
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.google.gson.reflect.TypeToken
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 import java.util.concurrent.TimeUnit
 
 /**
@@ -34,6 +40,8 @@ import java.util.concurrent.TimeUnit
  */
 const val LED = "BCM17"
 const val SENSOR = "BCM18"
+const val CONTROL_LIST = "ControlList"
+private val TAG = MainActivity::class.java.simpleName
 class MainActivity : Activity() {
 
     val noeRecording = false
@@ -41,11 +49,15 @@ class MainActivity : Activity() {
     val ledGpio = manager.openGpio(LED)
     val sensorGpio = manager.openGpio(SENSOR)
     var gpioEdgeList: MutableList<Pair<Boolean, Long>>? = null
-    val controlList = mutableListOf<MutableList<Pair<Boolean, Long>>>()
+    lateinit var controlList: MutableList<MutableList<Pair<Boolean, Long>>>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var json = getSharedPreferences(TAG, Context.MODE_PRIVATE)?.getString(CONTROL_LIST, null)
+        val gson = GsonBuilder().setPrettyPrinting().create()
+        val listType = object : TypeToken<MutableList<MutableList<Pair<Boolean, Long>>>>() {}.type
+        controlList = gson.fromJson(json, listType)
         button.setOnClickListener {
             gpioEdgeList = mutableListOf()
             val dialog = AlertDialog.Builder(this)
@@ -67,9 +79,12 @@ class MainActivity : Activity() {
 
     }
 
-    private fun recordIr() {
-        while (true) {
-
-        }
+    private fun setSharedPreference(controls: List<Pair<Boolean, Long>>) {
+        val sharedPreferences = getSharedPreferences(TAG, Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        val gson = Gson()
+        val json = gson.toJson(controls)
+        editor.putString(CONTROL_LIST, json)
+        editor.apply()
     }
 }
